b72804d39b247aad93e17866679ae6cdf8e7c6f9
Azizul Hoque <azhkingshakil@gmail.com>
2025-10-22 04:42:26 +0530

    __init__.py

--- ml_model/__init__.py
+++ ml_model/__init__.py
@@ -1,5 +1,1 @@
 # ml_model/__init__.py
-# This file is intentionally blank.
-# It tells Python that the 'ml_model' directory
-# should be treated as a package, allowing
-# us to import modules from it.
